538702d68518073072c26af9e5bf3bbfd8464abc
Miles Cranmer <miles.cranmer@gmail.com>
2019-03-20 14:23:02 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pydelfi'
-copyright = u'2017, justinalsing'
+copyright = u'2019, justinalsing'
 author = u'justinalsing'
 
 # The version info for the project you're documenting, acts as replacement for
