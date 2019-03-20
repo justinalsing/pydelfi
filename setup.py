d187a0db81aeb80c341f76b4d713e9e8a0706d59
MilesCranmer <miles.cranmer@gmail.com>
2019-03-20 10:20:42 -0400

    Add setup.py and more docs files

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import sys
+
+setup(name='pydelfi',
+      version='v0.1',
+      description='LFI in TensorFlow',
+      author='Justin Alsing',
+      author_email='benbarsdell@gmail.com',
+      url='https://github.com/justinalsing/pydelfi',
+      packages=find_packages(),
+      install_requires=[
+          "tensorflow>=v1.1.0",
+      ])
+
