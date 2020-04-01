43f1309453aa0f76821bb4bb0c0f552fb2f0dabc
Justin Alsing <j.a.alsing@gmail.com>
2020-04-01 10:21:29 +0200

    added old MADE and MAF temporarily for testing

--- pydelfi/ndes.py
+++ pydelfi/ndes.py
@@ -514,3 +514,277 @@ class MixtureDensityNetwork(tfd.Distribution):
         if squeeze:
             samples = tf.squeeze(samples, 1)
         return samples
+
+
+
+class ConditionalGaussianMADEJustin(tf.keras.Model):
+    """
+    Implements a gaussian MADE for modeling a conditional density p(d|\theta) (d="data", \theta="parameters")
+    """
+    
+    def __init__(self, n_parameters, n_data, n_hidden=[50,50], activations=[tf.tanh, tf.tanh], output_order='sequential', mode='sequential'):
+        """
+        Constructor.
+        :param n_parameters: number of (conditional) inputs
+        :param n_data: number of outputs (ie dimensionality of distribution you're parameterizing)
+        :param n_hidden: list with number of hidden units for each hidden layer
+        :param activations: activation functions for each hidden layer
+        """
+        
+        # super
+        super(ConditionalGaussianMADE, self).__init__()
+
+        # dimension of data and parameter spaces
+        self.n_parameters = n_parameters
+        self.n_data = n_data
+        
+        # network architecture
+        self.n_hidden = n_hidden
+        self.activations = activations
+        
+        # mode
+        self.mode = mode
+        
+        # degrees
+        degrees = self.create_degrees(output_order)
+        
+        # masks
+        self.Ms, self.Mmp = self.create_masks(degrees)
+        
+        # weights and biases
+        self.Ws = []
+        self.bs = []
+
+        self.n_units = np.concatenate(([self.n_data], self.n_hidden))
+        
+        self.Wx = tf.Variable(tf.random.normal([self.n_parameters, self.n_hidden[0]], 0., np.sqrt(1./(self.n_parameters + 1))), name="Wx" )
+
+        for l, (N0, N1) in enumerate(zip(self.n_units[:-1], self.n_units[1:])):
+
+            W = tf.Variable(tf.random.normal([N0, N1], 0., np.sqrt(1./(1+N0))), name="W"+str(l))
+            b = tf.Variable(tf.zeros([1, N1]), name="b"+str(l))
+            self.Ws.append(W)
+            self.bs.append(b)
+
+        self.Wm = tf.Variable(tf.random.normal([self.n_units[-1], self.n_data], 0., np.sqrt(1./(self.n_units[-1] + 1))), name="Wm" )
+        self.Wp = tf.Variable(tf.random.normal([self.n_units[-1], self.n_data], 0., np.sqrt(1./(self.n_units[-1] + 1))), name="Wp" )
+        self.bm = tf.Variable(tf.zeros([1, self.n_data]), name="bm")
+        self.bp = tf.Variable(tf.zeros([1, self.n_data]), name="bp")
+        
+        # output order
+        self.output_order = degrees[0]
+    
+    @tf.function
+    def call(self, data, parameters):
+        
+        # pass through layers of the network:
+        
+        # first layer
+        h = self.activations[0](tf.matmul(parameters, self.Wx) + tf.matmul(data, self.Ms[0] * self.Ws[0]) + self.bs[0])
+        
+        # subsequent hidden layers
+        for l, (M, W, b) in enumerate(zip(self.Ms[1:], self.Ws[1:], self.bs[1:])):
+            h = self.activations[l+1](tf.matmul(h, M * W) + b)
+        
+        # output means
+        mu = tf.add(tf.matmul(h, self.Mmp * self.Wm), self.bm)
+
+        # output log precisions
+        logp = tf.add(tf.matmul(h, self.Mmp * self.Wp), self.bp)
+        
+        # return shift and log_scale
+        return mu, logp
+    
+    @tf.function
+    def u(self, data, parameters):
+        
+        # shift and log_scale
+        mu, logp = self.call(data, parameters)
+        
+        # random numbers
+        u = tf.exp(0.5 * logp) * (data - mu)
+        
+        return u
+    
+    @tf.function
+    def log_prob(self, data, parameters):
+        
+        # shift and log_scale
+        mu, logp = self.call(data, parameters)
+        
+        # random numbers
+        u = tf.exp(0.5 * logp) * (data - mu)
+        
+        # log density
+        return tf.multiply(-0.5, self.n_data * np.log(2 * np.pi) + tf.reduce_sum(u**2 - logp, axis=1))
+    
+    @tf.function
+    def prob(self, data, parameters):
+        
+        # shift and log_scale
+        mu, logp = self.call(data, parameters)
+        
+        # random numbers
+        u = tf.exp(0.5 * logp) * (data - mu)
+        
+        # log density
+        return tf.exp(tf.multiply(-0.5, self.n_data * np.log(2 * np.pi) + tf.reduce_sum(u**2 - logp, axis=1)))
+
+    
+    def create_degrees(self, input_order):
+        """
+        Generates a degree for each hidden and input unit. A unit with degree d can only receive input from units with
+        degree less than d.
+        :param n_hidden: a list with the number of hidden units
+        :param input_order: the order of the inputs; can be 'random', 'sequential', or an array of an explicit order
+        :return: list of degrees
+        """
+
+        degrees = []
+
+        # create degrees for inputs
+        if isinstance(input_order, str):
+
+            if input_order == 'random':
+                degrees_0 = np.arange(1, self.n_data + 1)
+                rng.shuffle(degrees_0)
+
+            elif input_order == 'sequential':
+                degrees_0 = np.arange(1, self.n_data + 1)
+
+            else:
+                raise ValueError('invalid output order')
+
+        else:
+            input_order = np.array(input_order)
+            assert np.all(np.sort(input_order) == np.arange(1, self.n_data + 1)), 'invalid input order'
+            degrees_0 = input_order
+        degrees.append(degrees_0)
+
+        # create degrees for hiddens
+        if self.mode == 'random':
+            for N in self.n_hidden:
+                min_prev_degree = min(np.min(degrees[-1]), self.n_data - 1)
+                degrees_l = rng.randint(min_prev_degree, self.n_data, N)
+                degrees.append(degrees_l)
+
+        elif self.mode == 'sequential':
+            for N in self.n_hidden:
+                degrees_l = np.arange(N) % max(1, self.n_data - 1) + min(1, self.n_data - 1)
+                degrees.append(degrees_l)
+
+        else:
+            raise ValueError('invalid mode')
+
+        return degrees
+
+    def create_masks(self, degrees):
+        """
+        Creates the binary masks that make the connectivity autoregressive.
+        :param degrees: a list of degrees for every layer
+        :return: list of all masks, as theano shared variables
+        """
+
+        Ms = []
+
+        for l, (d0, d1) in enumerate(zip(degrees[:-1], degrees[1:])):
+            M = d0[:, np.newaxis] <= d1
+            M = tf.constant(M, dtype=dtype, name='M' + str(l+1))
+            Ms.append(M)
+
+        Mmp = degrees[-1][:, np.newaxis] < degrees[0]
+        Mmp = tf.constant(Mmp, dtype=dtype, name='Mmp')
+
+        return Ms, Mmp
+
+
+class ConditionalMaskedAutoregressiveFlowJustin(tf.keras.Model):
+    """
+    Conditional Masked Autoregressive Flow.
+    """
+
+    def __init__(self, n_parameters, n_data, n_hidden=[50,50], activations=[tf.tanh, tf.tanh], n_mades=5,
+                 output_order='sequential', mode='sequential'):
+        """
+        Constructor.
+        :param n_parameters: number of (conditional) inputs
+        :param n_data: number of outputs
+        :param n_hidden: list with number of hidden units for each hidden layer
+        :param activations: tensorflow activation functions
+        :param n_mades: number of mades in the flow
+        :param output_order: order of outputs of last made
+        :param mode: strategy for assigning degrees to hidden nodes: can be 'random' or 'sequential'
+        """
+        
+        # super
+        super(ConditionalMaskedAutoregressiveFlow, self).__init__()
+
+        # save input arguments
+        self.n_parameters = n_parameters
+        self.n_data = n_data
+        self.n_hidden = n_hidden
+        self.activations = activations
+        self.n_mades = n_mades
+        self.mode = mode
+
+        self.mades = []
+
+        # create some MADEs
+        for i in range(n_mades):
+            with tf.name_scope("made_" + str(i+1)):
+                self.mades.append(ConditionalGaussianMADE(n_parameters, n_data, n_hidden, activations, output_order, mode))
+            output_order = output_order if output_order is 'random' else self.mades[-1].output_order[::-1]
+
+        self.output_order = self.mades[0].output_order
+            
+    @tf.function
+    def call(self, data, parameters):
+        
+        u = [data]
+        
+        # loop through the MADEs
+        for i in range(self.n_mades):
+            # update state
+            u.append(self.mades[i].u(u[-1], parameters))
+        
+        # transformed vector
+        return u[-1]
+    
+    @tf.function
+    def log_prob(self, data, parameters):
+        
+        u = [data]
+        logdet_dudy = tf.zeros(data.shape[0])
+        
+        # loop through the MADEs
+        for i in range(self.n_mades):
+
+            # update jacobian
+            _, logp = self.mades[i](u[-1], parameters)
+            
+            # update state
+            u.append(self.mades[i].u(u[-1], parameters))
+            
+            logdet_dudy = logdet_dudy + 0.5 * tf.reduce_sum(logp, axis=1)
+        
+        # log density
+        return -0.5 * self.n_data * np.log(2 * np.pi) - 0.5 * tf.reduce_sum(u[-1] ** 2, axis=1) + logdet_dudy
+
+    @tf.function
+    def prob(self, data, parameters):
+        
+        u = [data]
+        logdet_dudy = tf.zeros(data.shape[0])
+        
+        # loop through the MADEs
+        for i in range(self.n_mades):
+
+            # update jacobian
+            _, logp = self.mades[i](u[-1], parameters)
+            logdet_dudy = logdet_dudy + 0.5 * tf.reduce_sum(logp, axis=1)
+
+            # update state
+            u.append(self.mades[i].u(u[-1], parameters))
+    
+        # likelihood
+        return tf.exp(-0.5 * self.n_data * np.log(2 * np.pi) - 0.5 * tf.reduce_sum(u[-1] ** 2, axis=1) + logdet_dudy)
