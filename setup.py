285ea18c2e6b1c16eaa0b1a1ed9715b86c114483
Justin Alsing <j.a.alsing@gmail.com>
2020-04-01 16:18:50 +0200

    added authors to the setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 setup(name='pydelfi',
       version='v0.2',
       description='LFI in TensorFlow',
-      author='Justin Alsing',
+      author='Justin Alsing, Tom Charnock, Stephen Feeney',
       url='https://github.com/justinalsing/pydelfi',
       packages=find_packages(),
       install_requires=[
